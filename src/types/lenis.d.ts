f5b8090edbf195c92f3dc610481db2604ac6514d
Ayoub <ayoubhamdi1746@gmail.com>
2026-05-15 22:14:23 +0100

    fix: add lenis types and nprogress @types

---
+++ src/types/lenis.d.ts
@@ -0,0 +1,12 @@
+declare module "lenis" {
+  interface LenisOptions {
+    duration?: number;
+    easing?: (t: number) => number;
+    smoothWheel?: boolean;
+  }
+  export default class Lenis {
+    constructor(options?: LenisOptions);
+    raf(time: number): void;
+    destroy(): void;
+  }
+}
